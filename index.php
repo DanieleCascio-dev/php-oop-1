d3434e8478f361607213cec14a5053892dd454d5
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-01-08 16:02:06 +0100

    display list of movie

--- index.php
+++ index.php
@@ -6,7 +6,38 @@ $avatar = new Movie("Avatar", "2h 22m","Sci-Fi","2009","8");
 $titanic = new Movie ("Titanic","3h 30m", "Romantic","1997","9");
 
 
+$movies = [
+  $avatar,
+  $titanic,
+  $interstellar = new Movie("Intersetllar", "2h 49m", "Sci-fi", "2014","8,5"),
+
+];
+
 var_dump($avatar);
 echo $avatar->showRating();
 
 var_dump($titanic);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<ul>
+  <?php foreach($movies as $movie){ ?>
+  <li>
+    <?php echo $movie->title ?>
+    <?php echo $movie->genre ?>
+    <?php echo $movie->showRating() ?>
+  </li>
+  <?php } ?>
+</ul>
+  
+</body>
+</html>
